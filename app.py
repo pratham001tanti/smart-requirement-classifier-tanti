366286c4022767ba9332b68becd0b5de285dc3ae
pratham001tanti <prathamtanti2305@gmail.com>
2025-06-12 12:19:25 +0200

    "Removal of unnecessary files"

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ import os
 from dotenv import load_dotenv
 import streamlit as st
 import pandas as pd
+from azure.core.credentials import AzureKeyCredential
 load_dotenv()
+
 
 client = AzureOpenAI(azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
 api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
 api_key=os.getenv("AZURE_OPENAI_KEY"))
-import streamlit as st
-import pandas as pd
-from azure.core.credentials import AzureKeyCredential
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
+
+
+
 
 deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT")
 
